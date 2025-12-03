20d84d63de0c11a3540181a691690179d3a0acd4
Kristina <kristinarabova.36@gmail.com>
2025-12-03 20:30:43 +0500

    Commit2delete234

--- src/main/java/org/example/carlabfulls/CarRepository.java
+++ src/main/java/org/example/carlabfulls/CarRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "https://labtest15.netlify.app/")
+@CrossOrigin(origins = "https://labtest15.netlify.app")
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
--- src/main/java/org/example/carlabfulls/SecurityConfig.java
+++ src/main/java/org/example/carlabfulls/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://labtest15.netlify.app"   // твой фронт
+                "https://labtest15.netlify.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
