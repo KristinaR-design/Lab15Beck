04bc64bec2acc9628fd1638525b5e5efe6b5cfdf
Kristina <kristinarabova.36@gmail.com>
2025-12-03 19:19:25 +0500

    fghjklоарпав

--- src/main/java/org/example/carlabfulls/SecurityConfig.java
+++ src/main/java/org/example/carlabfulls/SecurityConfig.java
@@ -28,11 +28,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
@@ -41,9 +36,13 @@ public class SecurityConfig {
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        // логин и регистрация открыты
+                        .requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                        // список машин открытый
+                        .requestMatchers(HttpMethod.GET, "/api/cars/**").permitAll()
+                        // префлайт CORS
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        // всё остальное требует JWT
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -54,13 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
-
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://labtest15.netlify.app"
         ));
-
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
